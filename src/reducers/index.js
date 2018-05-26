79dc66df0fb52a8a4604292a2ce72ffe7e6e908b
keeratis <33577830+keeratis@users.noreply.github.com>
2018-05-26 21:08:04 +0700

    Delete index.js

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import CounterReducer from "./reducer_counter";
-
-const rootReducer = combineReducers({
-  counter: CounterReducer
-});
-// tell redux how to create application state
